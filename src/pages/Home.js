56ec77a577ae424b0f47f0b9cc5db894654b74b0
Mounia Belkheir <mouniabelkheir@outlook.com>
2023-10-09 14:32:10 +0100

    completed assignment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,23 @@
-import { movies } from "../data";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import Movies from './Movies';
+import Directors from './Directors';
+import Actors from './Actors';
 
-function Home() {
-
+function App() {
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
   );
-};
+}
 
-export default Home;
+export default App;
